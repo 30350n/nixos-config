00e1c2e93a5a36848fd60e551d14ce4822ef1e7f
Bobbe <maxschlecht@web.de>
2024-05-20 20:48:01 +0200

    Set timezone

--- common/base-configuration.nix
+++ common/base-configuration.nix
@@ -6,8 +6,7 @@
 
     networking.networkmanager.enable = true;
 
-    # Set your time zone.
-    # time.timeZone = "Europe/Amsterdam";
+    time.timeZone = "Europe/Berlin";
 
     # Select internationalisation properties.
     # i18n.defaultLocale = "en_US.UTF-8";
