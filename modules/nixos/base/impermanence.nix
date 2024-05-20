40aa5f8f89caa2b0e2e8752f773ff281e1cb995a
Bobbe <maxschlecht@web.de>
2024-05-20 19:52:18 +0200

    Keep root VSCodium .config files

--- modules/nixos/base/impermanence.nix
+++ modules/nixos/base/impermanence.nix
@@ -14,6 +14,7 @@
             "/var/log"
             "/root/.ssh"
             "/root/.cache/pre-commit"
+            "/root/.config/VSCodium"
         ];
         files = [
             "/etc/machine-id"
