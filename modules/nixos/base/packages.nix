2335edd72fbbae738443484e2f0a6b4feedf8101
Bobbe <maxschlecht@web.de>
2024-05-20 19:52:18 +0200

    Add custom bash prompt

--- modules/nixos/base/packages.nix
+++ modules/nixos/base/packages.nix
@@ -11,6 +11,15 @@
     ];
 
     programs = {
+        bash.promptInit = ''
+            PROMPT_COMMAND='GIT_PS1_CMD=$(__git_ps1 " (%s)")'
+            if [[ $(id -u) == 0 ]]; then
+                PS1='\w\[\e[33;1m\]''${GIT_PS1_CMD}\n\[\e[31;1m\]в\[\e[0m\] '
+            else
+                PS1='\w\[\e[33;1m\]''${GIT_PS1_CMD}\n\[\e[32;1m\]в\[\e[0m\] '
+            fi
+        '';
+
         command-not-found.enable = false;
 
         git = {
