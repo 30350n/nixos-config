18baf741d26ecccc402bc91a03157f466f442225
Bobbe <maxschlecht@web.de>
2024-05-25 20:54:26 +0200

    Move networking settings to networking.nix
    Generation 428 2024-06-03 19:41:23 24.05.20240531.805a384

--- modules/nixos/base/configuration.nix
+++ modules/nixos/base/configuration.nix
@@ -3,20 +3,10 @@
         ./boot.nix
         ./fish.nix
         ./impermanence.nix
+        ./networking.nix
         ./locale.nix
         ./packages.nix
     ];
-
-    networking.networkmanager.enable = true;
-    networking.enableIPv6 = false;
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
-
-    # Enable CUPS to print documents.
-    # services.printing.enable = true;
 
     security.rtkit.enable = true;
     services.pipewire = {
---
+++ modules/nixos/base/networking.nix
@@ -0,0 +1,15 @@
+{
+    networking = {
+        networkmanager.enable = true;
+        enableIPv6 = false;
+    };
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+    # Enable CUPS to print documents.
+    # services.printing.enable = true;
+}
