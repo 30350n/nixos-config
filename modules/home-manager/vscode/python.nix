5aeda4f20a0e014532bd584b849daf48b19a6768
Bobbe <maxschlecht@web.de>
2024-05-25 20:54:26 +0200

    Add jupyter vscode extension

--- modules/home-manager/vscode/python.nix
+++ modules/home-manager/vscode/python.nix
@@ -2,6 +2,7 @@
     programs.vscode = {
         extensions = with pkgs.vscode-extensions; [
             ms-python.python
+            ms-toolsai.jupyter
             (import ./marketplace-extensions/blender-development.nix {inherit pkgs;})
         ];
     };
