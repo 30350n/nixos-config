c0fd0db8c06b8d5094a38b81eb71ea1618057549
Bobbe <maxschlecht@web.de>
2024-07-14 23:54:55 +0200

    Add python isort and autopep8 extensions to vscode

--- modules/home-manager/vscode/python.nix
+++ modules/home-manager/vscode/python.nix
@@ -2,9 +2,19 @@
     programs.vscode = {
         extensions = with pkgs.vscode-extensions; [
             ms-python.python
+            ms-python.isort
             ms-toolsai.jupyter
+            (import ./marketplace-extensions/autopep8.nix {inherit pkgs;})
             (import ./marketplace-extensions/vscode-pylance {inherit pkgs;})
             (import ./marketplace-extensions/blender-development.nix {inherit pkgs;})
         ];
+
+        userSettings = {
+            "[python]" = {
+                "editor.codeActionsOnSave" = {
+                    "source.organizeImports" = "always";
+                };
+            };
+        };
     };
 }
