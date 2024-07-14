70df44d896539ca0d7ce2b559a72102cb9240a9e
Bobbe <maxschlecht@web.de>
2024-07-14 21:47:13 +0200

    Patch jj to commit interactively by default

--- packages/jujutsu/default.nix
+++ packages/jujutsu/default.nix
@@ -2,6 +2,7 @@
 pkgs.unstable.jujutsu.overrideAttrs (
     prevAttrs: {
         patches = [
+            ./interactive-commit-by-default.patch
             ./max-message-length.patch
         ];
         doCheck = false;
