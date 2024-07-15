5824d6bdac57d1b4b77493c6f62945397442a030
Bobbe <maxschlecht@web.de>
2024-07-15 02:34:41 +0200

    Use official version of github-markdown-preview

--- modules/home-manager/vscode/markdown.nix
+++ modules/home-manager/vscode/markdown.nix
@@ -2,7 +2,7 @@
     programs.vscode = {
         extensions = with pkgs.vscode-extensions; [
             davidanson.vscode-markdownlint
-            (import ./marketplace-extensions/markdown-preview-github-styles.nix {inherit pkgs;})
+            pkgs.vscode-extensions.bierner.github-markdown-preview
         ];
     };
 }
--- modules/home-manager/vscode/marketplace-extensions/markdown-preview-github-styles.nix
+++
@@ -1,7 +0,0 @@
-{pkgs}:
-pkgs.vscode-utils.extensionFromVscodeMarketplace {
-    name = "markdown-preview-github-styles";
-    publisher = "bierner";
-    version = "2.0.4";
-    sha256 = "jJulxvjMNsqQqmsb5szQIAUuLWuHw824Caa0KArjUVw=";
-}
