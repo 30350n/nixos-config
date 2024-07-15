cbc62bba3c06ebdb8a2ae5b6f032e408057913ef
Bobbe <maxschlecht@web.de>
2024-07-15 02:34:41 +0200

    Disable MD014, MD033, MD041 rules in markdown linter
    Generation 527 2024-07-15 02:44:59 24.05.20240712.f12ee5f

--- modules/home-manager/vscode/markdown.nix
+++ modules/home-manager/vscode/markdown.nix
@@ -4,5 +4,13 @@
             davidanson.vscode-markdownlint
             pkgs.vscode-extensions.bierner.github-markdown-preview
         ];
+
+        userSettings = {
+            "markdownlint.config" = {
+                "MD014" = false; # commands-show-output
+                "MD033" = false; # no-inline-html
+                "MD041" = false; # first-line-heading/first-line-h1
+            };
+        };
     };
 }
