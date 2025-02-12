ffb791ec5bdec173437887cd802ed47c1172c66d
Bobbe <maxschlecht@web.de>
2025-02-12 19:30:46 +0100

    Add latex-utilities vscode extension

--- modules/home-manager/vscode/latex.nix
+++ modules/home-manager/vscode/latex.nix
@@ -2,6 +2,7 @@
     programs.vscode = {
         extensions = with pkgs.vscode-extensions; [
             james-yu.latex-workshop
+            (import ./marketplace-extensions/latex-utilities.nix {inherit pkgs;})
         ];
     };
 }
---
+++ modules/home-manager/vscode/marketplace-extensions/latex-utilities.nix
@@ -0,0 +1,7 @@
+{pkgs}:
+pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "latex-utilities";
+    publisher = "tecosaur";
+    version = "0.4.14";
+    sha256 = "GsbHzFcN56UbcaqFN9s+6u/KjUBn8tmks2ihK0pg3Ds=";
+}
