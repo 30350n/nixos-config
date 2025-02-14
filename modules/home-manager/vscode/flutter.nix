189c892dbe32c277a5884d17bdd5360eb72a1cc7
Bobbe <maxschlecht@web.de>
2025-02-14 17:55:05 +0100

    Add i18n-ally vscode extension for flutter development

--- modules/home-manager/vscode/flutter.nix
+++ modules/home-manager/vscode/flutter.nix
@@ -3,10 +3,14 @@
         extensions = with pkgs.vscode-extensions; [
             dart-code.flutter
             dart-code.dart-code
+            lokalise.i18n-ally
         ];
 
         userSettings = {
             "dart.checkForSdkUpdates" = false;
+
+            "i18n-ally.theme.annotation" = "#F5AB8E";
+            "i18n-ally.theme.annotationBorder" = "#F5AB8E40";
         };
     };
 }
