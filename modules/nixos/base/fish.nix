eb907136cf40d181e3513bb8e72e4c3ed3ffd302
Bobbe <maxschlecht@web.de>
2025-06-13 09:54:09 +0200

    Set $SHELL env var in fish

--- modules/nixos/base/fish.nix
+++ modules/nixos/base/fish.nix
@@ -17,6 +17,7 @@
         '';
     in {
         PAGER = "less -FrX";
+        SHELL = "${pkgs.fish}/bin/fish";
         NIX_BUILD_SHELL = nix-shell_fake_bash;
         DIRENV_LOG_FORMAT = "";
         DIRENV_WARN_TIMEOUT = 0;
