1760c8f6d0f8732e842ce3ba3c8250abd6d5f6e2
Bobbe <maxschlecht@web.de>
2025-08-29 16:01:06 +0200

    Add vscode ltex-plus extension, use tex-fmt to format latex code

--- modules/home-manager/vscode/latex.nix
+++ modules/home-manager/vscode/latex.nix
@@ -7,9 +7,14 @@
         extensions = with extensions.vscode-marketplace; [
             james-yu.latex-workshop
             tecosaur.latex-utilities
+            ltex-plus.vscode-ltex-plus
         ];
 
         userSettings = {
+            latex-workshop.formatting.latex = "tex-fmt";
+            latex-workshop.formatting.tex-fmt.path = "${pkgs.tex-fmt}/bin/tex-fmt";
+            latex-workshop.formatting.tex-fmt.args = ["--nowrap" "--tabsize" "4"];
+            ltex.ltex-ls.path = pkgs.ltex-ls-plus;
             "[latex]" = {
                 "editor.wordWrap" = "bounded";
             };
