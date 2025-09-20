84529f1fe5a3dbc012eba488e1ea4d46066199d4
Bobbe <maxschlecht@web.de>
2025-09-20 14:29:29 +0200

    Rename home-manager gtk.nix module to gnome.nix

--- users/bobbe/home.nix
+++ users/bobbe/home.nix
@@ -6,7 +6,7 @@
         ../../modules/home-manager/direnv.nix
         ../../modules/home-manager/fish.nix
         ../../modules/home-manager/git.nix
-        ../../modules/home-manager/gtk.nix
+        ../../modules/home-manager/gnome.nix
         ../../modules/home-manager/jujustu.nix
         ../../modules/home-manager/prusa-slicer.nix
         ../../modules/home-manager/vscode
