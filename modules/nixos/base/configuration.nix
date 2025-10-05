c67c7242be21b2fbe3bd66173ea2e97eeae77e70
Bobbe <maxschlecht@web.de>
2025-10-05 13:21:11 +0200

    Move sudo, nix base configurations to nixos-core

--- modules/nixos/base/configuration.nix
+++ modules/nixos/base/configuration.nix
@@ -1,4 +1,4 @@
-{flake-inputs, ...}: {
+{
     imports = [
         ./audio.nix
         ./boot.nix
@@ -11,24 +11,6 @@
 
     users.mutableUsers = false;
     users.users.root.hashedPasswordFile = "/persist/passwords/root";
-
-    security.sudo.extraConfig = ''
-        Defaults lecture = never
-        Defaults env_keep += "EDITOR"
-    '';
-
-    nix.registry.unstable = {
-        from = {
-            type = "indirect";
-            id = "unstable";
-        };
-        flake = flake-inputs.nixpkgs-unstable;
-    };
-    nix.nixPath = [
-        "nixpkgs=${flake-inputs.nixpkgs.outPath}"
-        "unstable=${flake-inputs.nixpkgs-unstable.outPath}"
-    ];
-    nix.settings.experimental-features = ["nix-command" "flakes"];
 
     system.stateVersion = "23.11";
 }
