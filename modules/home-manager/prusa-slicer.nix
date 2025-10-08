0bdff5d3f7ca931eb58a6f294e97bc7074f2f348
Bobbe <maxschlecht@web.de>
2025-10-08 15:00:58 +0200

    Fix prusa-slicer-config home-manager initialization script
    desktop - Generation 24 2025-10-08 15:08:17 25.05.20251006.20c4598

--- modules/home-manager/prusa-slicer.nix
+++ modules/home-manager/prusa-slicer.nix
@@ -5,15 +5,15 @@
     ...
 }: let
     git = pkgs.git.override {withSsh = true;};
-    url-http = "https://github.com/30350n/prusa-slicer-config.git";
-    url-ssh = "git@github.com:30350n/prusa-slicer-config.git";
+    url_http = "https://github.com/30350n/prusa-slicer-config.git";
+    url_ssh = "git@github.com:30350n/prusa-slicer-config.git";
     prusa_slicer_config = "${config.xdg.configHome}/PrusaSlicer";
 in {
     home.activation.initPrusaSlicerConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
         if [ ! -d "${prusa_slicer_config}/.git" ]; then
             run rm -rf "${prusa_slicer_config}"
-            run ${git}/bin/git clone "${url-http}" "${prusa_slicer_config}"
-            run ${git}/bin/git set-url origin ${url-ssh}
+            run ${git}/bin/git clone "${url_http}" "${prusa_slicer_config}"
+            run ${git}/bin/git -C "${prusa_slicer_config}" remote set-url origin ${url_ssh}
         fi
         if [ ! -d ${prusa_slicer_config}/.jj ]; then
             run ${pkgs.jujutsu}/bin/jj git init --colocate "${prusa_slicer_config}"
