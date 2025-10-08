cc8080cb434c83eff7c0079e579341d3bc9c2c16
Bobbe <maxschlecht@web.de>
2025-10-08 14:55:48 +0200

    Fix initializing prusa-slicer-config

--- modules/home-manager/prusa-slicer.nix
+++ modules/home-manager/prusa-slicer.nix
@@ -5,13 +5,15 @@
     ...
 }: let
     git = pkgs.git.override {withSsh = true;};
-    src = "git@github.com:30350n/prusa-slicer-config.git";
+    url-http = "https://github.com/30350n/prusa-slicer-config.git";
+    url-ssh = "git@github.com:30350n/prusa-slicer-config.git";
     prusa_slicer_config = "${config.xdg.configHome}/PrusaSlicer";
 in {
     home.activation.initPrusaSlicerConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
         if [ ! -d "${prusa_slicer_config}/.git" ]; then
             run rm -rf "${prusa_slicer_config}"
-            run ${git}/bin/git clone "${src}" "${prusa_slicer_config}"
+            run ${git}/bin/git clone "${url-http}" "${prusa_slicer_config}"
+            run ${git}/bin/git set-url origin ${url-ssh}
         fi
         if [ ! -d ${prusa_slicer_config}/.jj ]; then
             run ${pkgs.jujutsu}/bin/jj git init --colocate "${prusa_slicer_config}"
