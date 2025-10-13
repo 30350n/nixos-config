6cc210f6ecd499b90e63308b87e3036883a09308
Bobbe <maxschlecht@web.de>
2025-10-13 14:47:35 +0200

    Rename gnome-shell bottom-bar patch to panel-bottom

--- packages/gnome-shell/default.nix
+++ packages/gnome-shell/default.nix
@@ -1,4 +1,4 @@
 {pkgs}:
 pkgs.gnome-shell.overrideAttrs (prevAttrs: {
-    patches = (prevAttrs.patches or []) ++ [./bottom-bar.patch ./date-time-format.patch];
+    patches = (prevAttrs.patches or []) ++ [./panel-bottom.patch ./date-time-format.patch];
 })
