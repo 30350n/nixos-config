3cdddadb293c5f749ae26b7b3ebf30651bcf1cdf
Bobbe <maxschlecht@web.de>
2025-10-16 12:58:14 +0200

    Move xdg directory fixes to home-manager to fix pam_env errors
    desktop - Generation 127 2025-10-16 14:05:27 25.05.20251009.5da4a26

--- modules/home-manager/xdg.nix
+++ modules/home-manager/xdg.nix
@@ -14,4 +14,16 @@
         videos = "${home}/videos";
         templates = "${home}/.config/templates";
     };
+
+    home.sessionVariables = {
+        CARGO_HOME = "\"$XDG_DATA_HOME\"/cargo";
+        GRADLE_USER_HOME = "\"$XDG_DATA_HOME\"/gradle";
+        HISTFILE = "\"$XDG_DATA_HOME\"/.bash_history";
+        JAVA_TOOL_OPTIONS = "-Djava.util.prefs.userRoot=\"$XDG_DATA_HOME\"/java";
+        JULIA_DEPOT_PATH = "\"$XDG_DATA_HOME\"/julia:$JULIA_DEPOT_PATH";
+        JULIAUP_DEPOT_PATH = "\"$XDG_DATA_HOME\"/julia";
+        PYTHON_HISTORY = "\"$XDG_DATA_HOME\"/python/history";
+        PYTHONPYCACHEPREFIX = "\"$XDG_DATA_HOME\"/python";
+        PYTHONUSERBASE = "\"$XDG_DATA_HOME\"/python";
+    };
 }
--- modules/nixos/base/xdg.nix
+++ modules/nixos/base/xdg.nix
@@ -13,15 +13,5 @@
         XDG_CONFIG_HOME = "$HOME/.config";
         XDG_DATA_HOME = "$HOME/.local/share";
         XDG_STATE_HOME = "$HOME/.local/state";
-
-        CARGO_HOME = "$XDG_DATA_HOME/cargo";
-        GRADLE_USER_HOME = "$XDG_DATA_HOME/gradle";
-        HISTFILE = "$XDG_DATA_HOME/.bash_history";
-        JAVA_TOOL_OPTIONS = "-Djava.util.prefs.userRoot=$XDG_DATA_HOME/java";
-        JULIA_DEPOT_PATH = "$XDG_DATA_HOME/julia:$JULIA_DEPOT_PATH";
-        JULIAUP_DEPOT_PATH = "$XDG_DATA_HOME/julia";
-        PYTHON_HISTORY = "$XDG_DATA_HOME/python/history";
-        PYTHONPYCACHEPREFIX = "$XDG_DATA_HOME/python";
-        PYTHONUSERBASE = "$XDG_DATA_HOME/python";
     };
 }
