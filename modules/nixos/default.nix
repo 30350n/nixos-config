745de8d715f424240cbb4fe22fda2d998f04069f
Bobbe <maxschlecht@web.de>
2025-10-17 02:34:43 +0200

    Move fonts into default.nix

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -38,6 +38,20 @@
             ]))
         ];
 
+        fonts = {
+            packages = with pkgs; [
+                custom.ibm-plex
+                custom.nerd-fonts.blex-mono
+                custom.segoe-ui
+            ];
+
+            fontconfig.defaultFonts = {
+                serif = ["IBM Plex Serif"];
+                sansSerif = ["Segoe UI"];
+                monospace = ["IBM Plex Mono" "BlexMono Nerd Font"];
+            };
+        };
+
         hardware.bluetooth.enable = lib.mkOverride 999 config.custom.isLaptop;
 
         networking = {
