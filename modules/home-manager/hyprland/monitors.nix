8e0dc56f922ba6692194673a2643189b9dae3e67
Bobbe <maxschlecht@web.de>
2025-10-18 02:05:43 +0200

    Refactor hyprland monitor workspace config, Remove redundant nom package

--- modules/home-manager/hyprland/monitors.nix
+++ modules/home-manager/hyprland/monitors.nix
@@ -3,18 +3,16 @@
     nixosConfig,
     ...
 }: {
-    wayland.windowManager.hyprland.settings = {
+    wayland.windowManager.hyprland.settings = let
+        monitors = nixosConfig.custom.hyprland.monitors;
+    in {
         monitor =
-            if nixosConfig.custom.hyprland.monitors == []
-            then ",highres,auto,1"
-            else nixosConfig.custom.hyprland.monitors;
+            if monitors == []
+            then [",highres,auto,1"]
+            else monitors;
 
-        workspace = let
-            monitors =
-                map (monitor: builtins.head (lib.strings.split "," monitor))
-                nixosConfig.custom.hyprland.monitors;
-        in
-            map (i: "${builtins.toString (i + 1)},monitor:${builtins.elemAt monitors i}")
-            (builtins.genList (i: i) (builtins.length monitors - 1));
+        workspace =
+            lib.lists.imap1 (i: monitor: "${builtins.toString i},monitor:${monitor}")
+            (map (str: builtins.head (lib.strings.split "," str)) (lib.lists.dropEnd 1 monitors));
     };
 }
--- users/work/packages.nix
+++ users/work/packages.nix
@@ -15,5 +15,4 @@ with pkgs; [
 
     direnv
     custom.mkshell
-    nix-output-monitor
 ]
