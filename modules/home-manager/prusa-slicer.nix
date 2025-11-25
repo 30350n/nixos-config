fd37d92707cb6dc42114e2255e330169e380165d
Bobbe <maxschlecht@web.de>
2025-11-25 20:00:27 +0100

    Refactor prusa-slicer config init script

--- modules/home-manager/prusa-slicer.nix
+++ modules/home-manager/prusa-slicer.nix
@@ -4,19 +4,25 @@
     pkgs,
     ...
 }: let
-    git = pkgs.git.override {withSsh = true;};
     url_http = "https://github.com/30350n/prusa-slicer-config.git";
     url_ssh = "git@github.com:30350n/prusa-slicer-config.git";
     prusa_slicer_config = "${config.xdg.configHome}/PrusaSlicer";
+    init_prusa_slicer_config = pkgs.writeShellApplication {
+        name = "init-prusa-slicer-config";
+        runtimeInputs = with pkgs; [git jujutsu openssh];
+        text = ''
+            if [ ! -d "${prusa_slicer_config}/.git" ]; then
+                run rm -rf "${prusa_slicer_config}"
+                run git clone "${url_http}" "${prusa_slicer_config}"
+                run git -C "${prusa_slicer_config}" remote set-url origin ${url_ssh}
+            fi
+            if [ ! -d ${prusa_slicer_config}/.jj ]; then
+                run jj git init --colocate "${prusa_slicer_config}"
+            fi
+        '';
+    };
 in {
     home.activation.initPrusaSlicerConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        if [ ! -d "${prusa_slicer_config}/.git" ]; then
-            run rm -rf "${prusa_slicer_config}"
-            run ${git}/bin/git clone "${url_http}" "${prusa_slicer_config}"
-            run ${git}/bin/git -C "${prusa_slicer_config}" remote set-url origin ${url_ssh}
-        fi
-        if [ ! -d ${prusa_slicer_config}/.jj ]; then
-            run ${pkgs.jujutsu}/bin/jj git init --colocate "${prusa_slicer_config}"
-        fi
+        ${init_prusa_slicer_config}/bin/init-prusa-slicer-config
     '';
 }
