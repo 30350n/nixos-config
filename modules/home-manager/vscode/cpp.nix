90ef543a3ccf8662e9374a7022c194f1b9cc23e2
Bobbe <maxschlecht@web.de>
2025-12-18 12:09:01 +0100

    Remove platformio vscode extension as it depends on ms-vscode.cpptools

--- modules/home-manager/vscode/cpp.nix
+++ modules/home-manager/vscode/cpp.nix
@@ -7,20 +7,11 @@
         extensions = with extensions.vscode-marketplace; [
             llvm-vs-code-extensions.vscode-clangd
             twxs.cmake
-            platformio.platformio-ide
             bbenoist.qml
         ];
 
         userSettings = {
             "clangd.path" = "${pkgs.clang-tools}/bin/clangd";
-
-            "platformio-ide.useBuiltinPIOCore" = false;
-            "platformio-ide.disablePIOHomeStartup" = true;
-            "platformio-ide.useBuiltinPython" = false;
-
-            "files.exclude" = {
-                "**/.platformio/" = true;
-            };
         };
     };
 }
