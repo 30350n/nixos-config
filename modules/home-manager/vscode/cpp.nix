c4ea3cf537b0ecb098f2a4d2ac384e2a8c8ce78f
Bobbe <maxschlecht@web.de>
2026-02-14 13:49:06 +0100

    Add mesonbuild vscode extension

--- modules/home-manager/vscode/cpp.nix
+++ modules/home-manager/vscode/cpp.nix
@@ -7,11 +7,14 @@
         extensions = with extensions.vscode-marketplace; [
             llvm-vs-code-extensions.vscode-clangd
             twxs.cmake
+            mesonbuild.mesonbuild
             bbenoist.qml
         ];
 
         userSettings = {
             "clangd.path" = "${pkgs.clang-tools}/bin/clangd";
+            "mesonbuild.downloadLanguageServer" = false;
+            "mesonbuild.languageServerPath" = "${pkgs.mesonlsp}/bin/mesonlsp";
         };
     };
 }
